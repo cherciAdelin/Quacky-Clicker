56e6c9cf35439af11c92470018d0753cbdf66b64
Ade <adelinandreicia19@gmail.com>
2025-10-15 20:55:44 +0300

    day1 progress
    added an upgrade click value button, max_health growth of the egg based on how many eggs you have broken and a duck cursor

--- Scripts/egg.gd
+++ Scripts/egg.gd
@@ -1,13 +1,25 @@
 extends Area2D
 
+	## Variables
+
+## signals for when you click/break the egg that trigger functions
+## in the main script
 signal egg_pressed
 signal egg_broken
 
+## variables that are used to manipulate the health of the egg
 var egg_health := 100
-const max_egg_health := 100
+var max_egg_health := 100.0
+## variable used in a function that checks if you have broken the
+## required number of eggs. If the threshold is reached, the max health
+## of the egg grows by *10
+var egg_threshold := 10
+## connects variables and functions from the main script to this script
 var main
 
+## sprite has easy access to the sprite2d
 @onready var sprite = $egg_sprite
+##preloads the textures for the egg at the start of the program
 @onready var egg_textures := {
 	100:	preload("res://Assets/Sprites/Egg/Egg_full_202510151033_14760.png"),
 	75:		preload("res://Assets/Sprites/Egg/Egg_cracked1_202510151033_14758.png"),
@@ -16,6 +28,10 @@ var main
 	0:		preload("res://Assets/Sprites/Egg/Egg_broken_202510151033_14751.png"),
 }
 
+
+	##EGG
+
+## when you click the egg it triggers every command in this function
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if(event.is_action_pressed("left_click")):
 		egg_pressed.emit()
@@ -24,7 +40,11 @@ func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> voi
 		animate_tween()
 
 
-# egg health
+	## egg health
+
+## sets the health between -1 and max_egg_health which updates based on the number
+## of eggs broken, updates the egg sprite when the egg reaches 75%, 
+## 50%, 25%, 0% health and when the egg health drops below 0 it resets the egg
 func take_dmg(amount:int):
 	egg_health = clamp(egg_health - amount, -1, max_egg_health)
 	$egg_hp.text = "Egg health: " + str(egg_health)
@@ -34,21 +54,36 @@ func take_dmg(amount:int):
 		reset_egg()
 
 
-#sprite manipulation
+	## sprite manipulation
+
+## function that changes sprites based on the %health left which is calculated in 
+## the "percent" variable with the formula below
 func change_sprite():
-	var percent := egg_health
+	var percent: float
 	var threshholds = [0, 25, 50, 75, 100]
+	
+	percent = (egg_health / max_egg_health) * 100
 	
 	for i in threshholds:
 		if(percent <= i):
 			sprite.texture = egg_textures[i]
 			break
 
-func reset_egg():
-	egg_health = max_egg_health
-	$egg_hp.text = "Egg health: " + str(max_egg_health)
-	sprite.texture = egg_textures[max_egg_health]
 
+## function that resets the health and sprite of the egg and it
+## increases the %maxhealth of the egg if a certain number of eggs have been
+## broken
+func reset_egg():
+	if(main.eggs_broken >= egg_threshold):
+		max_egg_health *= 10
+		egg_threshold *= 10
+	egg_health = int(max_egg_health)
+	$egg_hp.text = "Egg health: " + str(egg_health)
+	sprite.texture = egg_textures[100]
+
+
+## function that changes the size of the sprite to create the illusion of the
+## egg bouncing when you click it
 func animate_tween():
 	var tween := create_tween()
 	tween.set_trans(Tween.TRANS_BACK)
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,22 +1,63 @@
 extends Node2D
 
-var points := 0
-var click_value := 10
-var eggs_broken := 0
+	## Variables
 
+## variables that hold the score, damage-per-click, number of eggs broken
+## the cost of the damage upgrades and the current upgrade dmg value
+var points := 0.0
+var click_value := 1.0
+var eggs_broken := 0
+var up_cost := 50
+var up_val := 1.0
+
+## first line creates the connection between this script and the egg script
+## second line updates every value and label the moment you run the program
 func _ready() -> void:
-	#DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN)
 	$Egg.main = self
 	update_ui()
 
+	## Egg functions
+
+## the signal you get from the egg collision shape when you click it
+## it increases the "Currency" you have and also updates the UI
 func _on_egg_egg_pressed() -> void:
 	points += click_value
 	update_ui()
 
+## updates every label on-screen with the latest values
 func update_ui():
-	$UI/Score.text = "Current points: " + str(points)
+	$UI/Score.text = "Current points: " + str(int(points))
+	$UI/Click_val.text = "Click value: " + str(click_value)
+	$UI/Upgrade1.text = "CLICK UPGRADE: " + str(up_cost) + " $$$"
 
+## the signal you get when the egg health drops below 0
+## it increases the number of eggs broken and provides a 20% bonus to your current points
 func _on_egg_egg_broken() -> void:
 	eggs_broken += 1
-	points += int(points*0.1)
+	points += int(points*0.2)
 	$UI/Score.text = "Congratulations! You have broken " + str(eggs_broken) + " eggs! Bonus points awarded!"
+
+	## Upgrade button functions
+
+## the signal you get when you press the upgrade button
+## it either tells you you don't have enough currency to upgrade or
+## subtracts the upgrade cost from your current currency, increases the cost of the next upgrade
+## increases the damage you deal to the egg and displays appropriate messages for each case
+func _on_upgrade_1_pressed() -> void:
+	if(points < up_cost):
+		$UI/Upgrade1.text = "Insufficient currency!"
+		await get_tree().create_timer(2.0).timeout
+		update_ui()
+	else:
+		points -= up_cost
+		up_cost += up_cost + 50
+		up_clickval_flat()
+		$UI/Upgrade1.text = "Upgrade succesful!"
+		await get_tree().create_timer(2.0).timeout
+		update_ui()
+
+## increases the damage you deal to the egg and increases the next upgrade value by 0.1
+func up_clickval_flat():
+	click_value += up_val
+	up_val += 0.1
+	update_ui()
